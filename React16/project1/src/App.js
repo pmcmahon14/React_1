e8188ae70224baf5da01946fe38ac38a97d75df9
Patrick McMahon <pmcmahon14@gmail.com>
2018-08-10 00:09:05 -0700

    created first app on App.js, works perfectly

--- React16/project1/src/App.js
+++ React16/project1/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -9,6 +9,7 @@ class App extends Component {
         <h1>Hi, I'm a React App</h1>
       </div>
     );
+    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
   }
 }
 
