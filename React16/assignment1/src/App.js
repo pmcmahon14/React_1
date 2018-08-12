c8c0d9bc46375f69ab6e0305d95bd25d77919683
Patrick McMahon <pmcmahon14@gmail.com>
2018-08-12 10:13:43 -0700

    added UserInput and UserOutput directories and a js page to each one

--- React16/assignment1/src/App.js
+++ React16/assignment1/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+    UserInput = <input type="text"/>
+
+    UserOutput = return (
+        <div>
+        <p>Paragraph 1</p>
+        <p>Paragraph 2</p>
+    </div>
+    );
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
       </div>
     );
   }
