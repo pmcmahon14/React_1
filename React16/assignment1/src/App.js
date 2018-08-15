ce2926afb18f595c245d540b7614785d21cc6026
Patrick McMahon <pmcmahon14@gmail.com>
2018-08-15 12:00:31 -0700

    finished assignment

--- React16/assignment1/src/App.js
+++ React16/assignment1/src/App.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import './App.css';
 
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+
 class App extends Component {
+    state = {
+        username: 'crazydude'
+    }
 
-    UserInput = <input type="text"/>
-
-    UserOutput = return (
-        <div>
-        <p>Paragraph 1</p>
-        <p>Paragraph 2</p>
-    </div>
-    );
-
-  render() {
+    usernameChangedHandler = (event) => {
+        this.setState({username: event.target.value})
+    }
+    render() {
     return (
       <div className="App">
-        
+          <UserInput changed={this.usernameChangedHandler}/>
+          <UserOutput userName={this.state.username}/>
+          <UserOutput userName={this.state.username}/>
+          <UserOutput userName="Dogs"/>
       </div>
     );
   }
