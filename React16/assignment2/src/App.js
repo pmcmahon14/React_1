70756b235a851b9f56d6ae7edabe174d7f381dd5
Patrick McMahon <pmcmahon14@gmail.com>
2018-08-16 22:28:09 -0700

    double check

--- React16/assignment2/src/App.js
+++ React16/assignment2/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Validation from './Validation/Validation';
 
 class App extends Component {
+    state = {
+        userInput: ''
+    };
+
+    inputChangedHandler = (event) => {
+        this.setState({userInput: event.target.value});
+    };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input
+            type='text'
+            onChange={this.inputChangedHandler}
+            value={this.state.userInput} />
+          <p>{this.state.userInput}</p>
+          <Validation inputLength={this.state.userInput.length} />
       </div>
     );
   }
