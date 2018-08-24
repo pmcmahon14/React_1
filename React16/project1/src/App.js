9ec1d57d913cb46cd3d1e9b21e5f855e82bf6a06
Patrick McMahon <pmcmahon14@gmail.com>
2018-08-23 21:29:41 -0700

    updated after radium installation

--- React16/project1/src/App.js
+++ React16/project1/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import Radium, {StyleRoot} from 'radium';
+//import Radium, {StyleRoot} from 'radium';
 
 class App extends Component {
     state = {
@@ -53,10 +53,6 @@ class App extends Component {
           border: '1px solid blue',
           padding: '8px',
           cursor: 'pointer',
-          ':hover': {
-              backgroundColor: 'lightgreen',
-              color: 'black'
-          }
       };
 
       let persons = null;
@@ -75,11 +71,6 @@ class App extends Component {
               </div>
           );
 
-          style.backgroundColor = 'red';
-          style[':hover'] = {
-              backgroundColor: 'lightgreen',
-              color: 'black'
-          }
       }
 
       const classes = [];
@@ -90,19 +81,17 @@ class App extends Component {
         classes.push('bold');  //classes = ['red', 'bold']
     }
     return (
-        <StyleRoot>
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
+          <h1>Hi, I'm a React App</h1>
         <p className={classes.join(' ')}>This is really working!</p>
           <button
               style={style}
               onClick={this.togglePersonsHandler}>Toggle Persons</button>
           {persons}
       </div>
-      </StyleRoot>
     );
     //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'))
   }
 }
 
-export default Radium(App);
+export default App;
