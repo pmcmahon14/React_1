54c4c8f3ff99a4e30b3403f31bdb681d24858707
Patrick McMahon <pmcmahon14@gmail.com>
2018-12-17 16:24:08 -0800

    styled to Sega font, problem with woff file type

--- robofriend/src/App.js
+++ robofriend/src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {robots} from './robots';
 import SearchBox from './SearchBox';
 import CardList from "./CardList";
+//import './App.css';
 
 class App extends Component {
     constructor() {
