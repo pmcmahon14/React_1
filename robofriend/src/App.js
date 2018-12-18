56391a9f2e8c00b54f794e5f4375e5765dda88f5
Patrick McMahon <pmcmahon14@gmail.com>
2018-12-17 20:09:07 -0800

    colored h1 header

--- robofriend/src/App.js
+++ robofriend/src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {robots} from './robots';
 import SearchBox from './SearchBox';
 import CardList from "./CardList";
-//import './App.css';
+import './App.css';
 
 class App extends Component {
     constructor() {
